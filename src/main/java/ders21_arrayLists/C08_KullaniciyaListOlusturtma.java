6b12f56b6bd3f84378742ddfffd9484d13db2923
Mustafa Lafcı <115083814+mustafalafci@users.noreply.github.com>
2022-10-25 18:45:11 +0300

    proje

---
+++ src/main/java/ders21_arrayLists/C08_KullaniciyaListOlusturtma.java
@@ -0,0 +1,31 @@
+package ders21_arrayLists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class C08_KullaniciyaListOlusturtma {
+    public static void main(String[] args) {
+
+
+        // Soru 2- Kullanicidan istedigi kadar isim alip,
+        // Q’ya bastiginda girdigi isimleri bize liste olarak dondurecek bir method olusturun.
+        System.out.println(ListeOlustur());
+
+    }
+    public  static List<String> ListeOlustur(){
+        List<String> isimler=new ArrayList<>();
+        Scanner scan =new Scanner(System.in);
+        String girilenIsim="";
+        while (!girilenIsim.equalsIgnoreCase("q")){
+            System.out.println("Lutfen listeye eklemek icin isim giriniz"+
+                    "\nBitirmek icin q ya basiniz");
+            girilenIsim= scan.nextLine();
+
+            if (!girilenIsim.equalsIgnoreCase("q")) {
+                isimler.add(girilenIsim);
+            }
+        }
+        return  isimler;
+    }
+ }
